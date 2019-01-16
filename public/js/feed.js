135a338546c09b76690a8eb0e3a02596c09ff705
Zoe Allen <zoeallen@dhcp-18-30-101-212.dyn.mit.edu>
2019-01-16 15:15:45 -0500

    Added composer and js for comments

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,4 +1,24 @@
 
+/*function submitDilemmaHandler() {
+  const data = {
+    title: "Should I do X thing?",
+    body: "Hello world, this is a fake new dilemma"
+  }
+  post('/api/dilemma', data);
+  console.log('this is working')
+}
+*/
+
+function composerAppears() {
+  let overlayComposer = document.getElementById('composer');
+  overlayComposer.style.display = "block";
+}
+
+function closeComposer() {
+  let overlayComposer = document.getElementById('composer');
+  overlayComposer.style.display = "none";
+}
+
 function submitDilemmaHandler() {
   const data = {
     title: "Should I do X thing?",
@@ -8,5 +28,73 @@ function submitDilemmaHandler() {
   console.log('this is working')
 }
 
-const newDilemmaSubmit = document.getElementById('new-dilemma');
-newDilemmaSubmit.addEventListener('click', submitDilemmaHandler);
+function expandDilemmas(){
+  let debateSection = document.getElementById('debate-section');
+  debateSection.style.display = "block";
+
+  let dCardExpandFooter = document.getElementById ('d-card-expand-footer');
+  dCardExpandFooter.style.display = "none";
+
+  let topComments = document.getElementById ('top-comments');
+  topComments.style.display = "none";
+}
+
+function createNewComment () {
+  const commentValue = document.getElementById('comment-field-yes').value;
+  console.log(commentValue);
+  document.getElementById('comment-field-yes').value = '';
+
+  const commentsDiv = document.getElementById('comment-yes');
+
+  const newComment = document.createElement('div');
+  newComment.className = 'comment-wrapper';
+  commentsDiv.appendChild(newComment);
+
+  const commentText = document.createElement('div');
+  commentText.innerText = commentValue;
+  commentText.className = 'comment-body';
+  newComment.appendChild(commentText);
+
+  const commentVote = document.createElement('div');
+  commentVote.className = 'comment-vote';
+  newComment.appendChild(commentVote);
+
+  const voteButton = document.createElement('div');
+  voteButton.className = 'vote-button';
+  commentVote.appendChild(voteButton);
+
+  const voteCount = document.createElement('div');
+  voteCount.innerText = '0';
+  voteCount.className = 'vote-count';
+  commentVote.appendChild(voteCount);
+
+  const commentMeta = document.createElement('div');
+  commentMeta.className = 'comment-meta';
+  newComment.appendChild(commentMeta);
+
+  const commentThumb = document.createElement('div');
+  commentThumb.className = 'comment-thumb';
+  commentMeta.appendChild(commentThumb);
+
+  const commentAuthor = document.createElement('span');
+  commentAuthor.className = 'comment-author';
+  commentAuthor.innerText = 'Quirky Lllama poste 1s ago'
+  commentMeta.appendChild(commentAuthor);
+
+}
+
+const newDilemma = document.getElementById('new-dilemma');
+/*newDilemmaSubmit.addEventListener('click', submitDilemmaHandler);*/
+newDilemma.addEventListener('click', composerAppears);
+
+const closeNewDilemma = document.getElementById('close');
+closeNewDilemma.addEventListener('click', closeComposer)
+
+const postDilemma = document.getElementById('post-button');
+postDilemma.addEventListener('click', submitDilemmaHandler);
+
+const expandDilemma = document.getElementById ('d-card-expand-footer');
+expandDilemma.addEventListener('click', expandDilemmas);
+
+const submitComment = document.getElementById('submit-comment-yes');
+submitComment.addEventListener('click', createNewComment)
