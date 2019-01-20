e47ce9c69aaaa36a8d34a6b07e93717c9c597811
Zoe Allen <zoeallen@Zoes-MacBook-Pro.local>
2019-01-20 00:01:24 -0500

    fixed minor issue with timestamp

--- public/js/dom-objects.js
+++ public/js/dom-objects.js
@@ -3,13 +3,13 @@ function timeSince(timeStamp) {
   var now = new Date(),
     secondsPast = (now.getTime() - timeStamp.getTime()) / 1000;
   if(secondsPast < 60){
-    return parseInt(secondsPast) + 's';
+    return parseInt(secondsPast) + 's' + " ago";
   }
   if(secondsPast < 3600){
-    return parseInt(secondsPast/60) + 'm';
+    return parseInt(secondsPast/60) + 'm' + " ago";
   }
   if(secondsPast <= 86400){
-    return parseInt(secondsPast/3600) + 'h';
+    return parseInt(secondsPast/3600) + 'h' + " ago";
   }
   if(secondsPast > 86400){
       day = timeStamp.getDate();
@@ -65,7 +65,7 @@ function dilemmaDOMObject (dilemmaJSON){
   const dTimestamp = document.createElement('div');
   dTimestamp.className = 'd-timestamp';
   if (dilemmaJSON.timestamp != null) {
-    dTimestamp.innerText = 'Posted ' + timeSince(new Date(dilemmaJSON.timestamp)) + " ago";
+    dTimestamp.innerText = 'Posted ' + timeSince(new Date(dilemmaJSON.timestamp));
   }
   
   dMeta.appendChild(dTimestamp);
@@ -360,7 +360,7 @@ function commentDOMObject (commentJSON) {
   
     const commentAuthor = document.createElement('span');
     commentAuthor.className = 'comment-author';
-    commentAuthor.innerText = commentJSON.creator_alias + ' posted ' + timeSince(new Date(commentJSON.timestamp)) + ' ago';
+    commentAuthor.innerText = commentJSON.creator_alias + ' posted ' + timeSince(new Date(commentJSON.timestamp));
     commentMeta.appendChild(commentAuthor);
 
     return commentDiv;
